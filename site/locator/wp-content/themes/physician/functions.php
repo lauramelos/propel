fd58374c80c89f7169be42eb8ca90177a377a803
Juan Facundo Suárez <facundo@svarez.com.ar>
2013-06-12 16:14:25 -0300

    removing menus

--- site/locator/wp-content/themes/physician/functions.php
+++ site/locator/wp-content/themes/physician/functions.php
@@ -3,8 +3,13 @@
   function my_remove_menu_pages() {
         remove_menu_page('themes.php');
         remove_menu_page('tools.php');
+        remove_menu_page('edit-comments.php');
+        remove_menu_page('plugins.php');
+        remove_menu_page('edit.php?post_type=page');
         remove_menu_page('users.php');
         remove_menu_page('options-general.php');
+        remove_menu_page('upload.php');
+        remove_submenu_page( 'index.php', 'update-core.php' );
 }
   add_action( 'admin_menu', 'my_remove_menu_pages' );
 
